91e5993a580b98f301840f85e9f75ba17e16918b
Rafael Henrique Gonçalves de Oliveira <rafael.h.g.oliveira@gmail.com>
2024-04-02 16:17:26 -0300

    starting frontend

--- order/models.py
+++ order/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from customer.models import User
 from product.models import Product, Base
 
-# Create your models here.
+
 class Order(Base):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
