3e922b1a6076abb40f5c0e941f149fb157225fda
Rafael Henrique Gonçalves de Oliveira <rafael.h.g.oliveira@gmail.com>
2024-04-18 22:27:55 -0300

    added products to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
+        "./product/templates/**/*.html", // Adicionando o diretório de templates em product/templates
         "./templates/**/*.html",
         "./base_templates/**/*.html",
         "./node_modules/flowbite/**/*.js",
