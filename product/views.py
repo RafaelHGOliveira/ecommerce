f4cccc4b56c47a1cd749cb7c82663ec7b55d1000
Rafael Henrique Gonçalves de Oliveira <rafael.h.g.oliveira@gmail.com>
2024-04-23 18:11:43 -0300

    showing categories on navbar on detail products page

--- product/views.py
+++ product/views.py
@@ -6,12 +6,12 @@ from django.core.paginator import Paginator
 
 
 def index(request):
+
     products = Product.objects.all().order_by('-id')
     categories = Category.objects.all().order_by('-id')
 
-    # TODO: configure pagination
-    paginator = Paginator(products, 50)
-    p = request.GET.get('p')
+    paginator = Paginator(products, 20)
+    p = request.GET.get('page')
     products = paginator.get_page(p)
 
     return render(request, 'index.html', {
@@ -21,9 +21,13 @@ def index(request):
 
 
 def product_detail(request, product_id):
+
     product = get_object_or_404(Product, id=product_id)
+    categories = Category.objects.all().order_by('-id')
+
     if not product.active:
         raise Http404()
     return render(request, 'product_detail.html', {
         'product': product,
+        'categories': categories,
     })
